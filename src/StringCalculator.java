6a0cab6b30e4074f10d905e94a1a76fee65aa629
Bibidi <byungdeuk.kim@gmail.com>
2022-12-31 09:28:40 +0900

    Refactoring 2

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,41 +1,42 @@
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
-    private ArrayList<Integer> numbers;
+    private String[] tokens;
+    private int[] numbers;
 
-    public StringCalculator(String expression) {
-        if (expression.startsWith("//")) {
-            StringTokenizer stringTokenizer = new StringTokenizer(expression);
-            char delimiter = stringTokenizer.nextToken().charAt(2);
-            String numberString = stringTokenizer.nextToken();
-
-            numberString = numberString.replace(delimiter, ' ');
-            stringTokenizer = new StringTokenizer(numberString);
-            while (stringTokenizer.hasMoreTokens()) {
-                int number = Integer.parseInt(stringTokenizer.nextToken());
-                if (number < 0) {
-                    throw new RuntimeException("음수가 포함되어 있습니다.");
-                }
-                numbers.add(number);
-            }
-        } else {
-            String numberString = expression;
-            numberString = numberString.replace(';', ' ');
-            numberString = numberString.replace(',', ' ');
-
-            StringTokenizer stringTokenizer = new StringTokenizer(numberString);
-            while (stringTokenizer.hasMoreTokens()) {
-                int number = Integer.parseInt(stringTokenizer.nextToken());
-                if (number < 0) {
-                    throw new RuntimeException("음수가 포함되어 있습니다.");
-                }
-                numbers.add(number);
-            }
-        }
+    public StringCalculator(String text) {
+        tokens = parse(text);
+        numbers = convert(tokens);
+        validate(numbers);
     }
 
-    public int sum() {
-        return numbers.stream().mapToInt(Integer::intValue).sum();
+    public int add() {
+        return Arrays.stream(numbers).sum();
+    }
+
+    private String[] parse(String text) {
+        String[] tokens;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = "[" + m.group(1) + "]";
+            tokens = m.group(2).split(customDelimiter);
+        } else {
+            tokens = text.split(",|:");
+        }
+        return tokens;
+    }
+
+    private int[] convert(String[] tokens) {
+        return Arrays.stream(tokens).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private void validate(int[] numbers) {
+        Arrays.stream(numbers).forEach(number -> {
+            if (number < 0) {
+                throw new RuntimeException("음수가 포함되어 있습니다");
+            }
+        });
     }
 }
--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -3,8 +3,56 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+
     @Test
-    public void test() {
-        System.out.println("This is test!!");
+    public void 지정구분자가_없는_문자열_계산() {
+        //given
+        String text = "1,3,5:7:9";
+        StringCalculator stringCalculator = new StringCalculator(text);
+
+        //when
+        int sum = stringCalculator.add();
+
+        //then
+        assertEquals(25, sum);
+    }
+
+    @Test
+    public void 지정구분자가_있는_문자열_계산() {
+        //given
+        String text = "//!\n1!3!5!7!9";
+        StringCalculator stringCalculator = new StringCalculator(text);
+
+        //when
+        int sum = stringCalculator.add();
+
+        //then
+        assertEquals(25, sum);
+    }
+
+    @Test
+    public void 구분자가_점인_경우_계산() {
+        //given
+        String text = "//.\n1.3.5.7.9";
+        StringCalculator stringCalculator = new StringCalculator(text);
+
+        //when
+        int sum = stringCalculator.add();
+
+        //then
+        assertEquals(25, sum);
+    }
+
+    @Test
+    public void 음수가_포함되어_있으면_오류가_발생해야한다() {
+        //given
+        String text = "-1,10,5,7,9";
+
+        //when
+        Exception exception = assertThrows(Exception.class,
+                () -> new StringCalculator(text));
+
+        //then
+        assertEquals("음수가 포함되어 있습니다", exception.getMessage());
     }
 }
